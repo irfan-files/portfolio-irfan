45597b90365823ab7de745f164fe0d8358e7c8b8
irfan <irfan.maulana@students.amikom.ac.id>
2021-03-24 01:33:20 +0700

    Update auth0.js

--- utils/auth0.js
+++ utils/auth0.js
@@ -1,19 +1,5 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
-// export default initAuth0({
-//   domain: "lanparty.us.auth0.com",
-//   clientId: "09LGKfPpr2cTv1MELzMYcdVuKYre6gAM",
-//   clientSecret:
-//     "BcJm64kqB_wNfqFFY5evEeu5EHOR0e5kozDN1o1F4dEJHy7j1IjPBEn2rmsgAjbW",
-//   secret: "BcJm64kqB_wNfqFFY5evEeu5EHOR0e5kozDN1o1F4dEJHy7j1IjPBEn2rmsgAjbW",
-//   scope: "openid profile",
-//   redirectUri: "http://localhost:3000/api/callback",
-//   postLogoutRedirectUri: "http://localhost:3000/",
-//   session: {
-//     cookieSecret: `LLIeDUaEHHbdttjcdflfkoGGJID4jY6e>?{823h9ru2423rf903eiohr2039}`,
-//   },
-// });
-
 export default initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
